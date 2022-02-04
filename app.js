1c93820e825953934e15d7bcb1613a656c482538
Difelice-e <difelice.emanuele@gmail.com>
2022-02-04 14:51:28 +0100

    sviluppo esercizio

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // Chiedere numero di chilometri 
+const kmNumber = prompt("inserisci il numero di kilometri del tuo viaggio");
+console.log(kmNumber)
+
 // chiedere età (bonus:chiedere data di nascita)
+const userAge = parseInt(prompt("inserisci la tua età"));
+console.log(userAge)
 
 // prezzo del biglietto: km * 0.21€ 
-// sconto possibile: 20% per età < 18 anni 
-// sconto possibile: 40% per età > 64 anni 
+let ticketPrice = kmNumber * 0.21;
+console.log(ticketPrice)
+
+
+if (userAge < 18) {        // sconto possibile: 20% per età < 18 anni
+    ticketPrice = ticketPrice * 0.8;
+} else if (userAge > 64) { // sconto possibile: 40% per età > 64 anni 
+    ticketPrice = ticketPrice * 0.6;
+}
+console.log(ticketPrice)
 
 // printare prezzo finale
+document.getElementById("ticket_price").innerHTML += ticketPrice;
+

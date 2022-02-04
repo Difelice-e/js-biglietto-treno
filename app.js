be7c5fdadc1455c9f2460a3f2d9e4cc83197756e
Difelice-e <difelice.emanuele@gmail.com>
2022-02-04 14:31:11 +0100

    preparazione struttura

---
+++ app.js
@@ -0,0 +1,8 @@
+// Chiedere numero di chilometri 
+// chiedere età (bonus:chiedere data di nascita)
+
+// prezzo del biglietto: km * 0.21€ 
+// sconto possibile: 20% per età < 18 anni 
+// sconto possibile: 40% per età > 64 anni 
+
+// printare prezzo finale
